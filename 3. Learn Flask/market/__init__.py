9e463ec940a27ca07e98704fbd7606ac330c9b73
Aryo SK <81485489+aryoeska@users.noreply.github.com>
2024-04-26 00:54:27 +0700

    login still error

--- 3. Learn Flask/market/__init__.py
+++ 3. Learn Flask/market/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from market import routes
+
 
 app = Flask(__name__) # set up out application
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:010202@localhost/market'
@@ -11,4 +11,6 @@ app.config['SECRET_KEY'] = '57a90d75d390273247c7506a'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+
+from market import routes
 
