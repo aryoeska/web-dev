8f6c917a77f76fa118289f57e2516a6333819686
Aryo SK <81485489+aryoeska@users.noreply.github.com>
2024-04-25 20:37:21 +0700

    create jfhakljsdhfjklafjikl

--- 3. Learn Flask/market/__init__.py
+++ 3. Learn Flask/market/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__) # set up out application
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:010202@localhost/market'
 db = SQLAlchemy(app)
 
+from market import routes
 
-if __name__ == '__main__':
-     app.run(debug=True, use_reloader=False)
