9899ab5286bc21b65f6b4b79733177c96c86bc45
Aryo SK <81485489+aryoeska@users.noreply.github.com>
2024-04-27 01:37:39 +0700

    login clear

--- 3. Learn Flask/market/models.py
+++ 3. Learn Flask/market/models.py
@@ -1,4 +1,5 @@
-from market import db, bcrypt, login_manager
+from market import db, login_manager
+from market import bcrypt
 from flask_login import UserMixin
 
 @login_manager.user_loader
@@ -12,6 +13,14 @@ class User(db.Model, UserMixin):
      password_hash = db.Column(db.String(length=60), nullable=False)
      budget = db.Column(db.Integer(), nullable=False, default=1000)
      items = db.relationship('Item', backref='owned_user', lazy=True)
+
+     @property
+     def prettier_budget(self):
+        if len(str(self.budget)) >= 4:
+            return f'{str(self.budget)[:-3]},{str(self.budget)[-3:]}$'
+        else:
+            return f"{self.budget}$"
+
 
      @property
      def password(self):
