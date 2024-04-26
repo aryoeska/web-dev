50728e51e55ba83194c2c43a9f20eba9308983f7
Aryo SK <81485489+aryoeska@users.noreply.github.com>
2024-04-27 02:29:37 +0700

    edit item column

--- 3. Learn Flask/market/routes.py
+++ 3. Learn Flask/market/routes.py
@@ -2,7 +2,7 @@ from market import app, db
 from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 
 @app.route('/')
 @app.route('/home')
@@ -10,6 +10,7 @@ def home_page():
      return render_template('home.html')
 
 @app.route('/market')
+@login_required
 def market_page():
      items = Item.query.all()
      return render_template('market.html', items=items)
@@ -23,6 +24,9 @@ def register_page():
                                 password = form.password1.data)
           db.session.add(user_to_create)
           db.session.commit()
+          login_user(user_to_create)
+          flash(f'Account created successfully! You are now logged in as : {user_to_create.username}', category='success')     
+
           return redirect(url_for('market_page'))
      if form.errors != {}:
           for err_msg in form.errors.values():
@@ -42,3 +46,9 @@ def login_page():
           else:
                flash('Username and password are not match! Please try again', category='danger')
      return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout_page():
+     logout_user()
+     flash('You have been logged out!', category='info')
+     return redirect(url_for('home_page'))
