6c96acad65f6c7659ecdf36d37924fd36424f774
Aryo SK <81485489+aryoeska@users.noreply.github.com>
2024-05-08 02:18:29 +0700

    learn flask articles

--- 5. Learn Flask II/app.py
+++ 5. Learn Flask II/app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5002, debug=True)
