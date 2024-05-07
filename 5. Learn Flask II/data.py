d8b5399fdc1d5874684ec8b586539eec7e998999
Aryo SK <81485489+aryoeska@users.noreply.github.com>
2024-05-08 02:33:35 +0700

    making articles pages

---
+++ 5. Learn Flask II/data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id' : 1,
+            'title' : 'Article One',
+            'body' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae provident vero magni architecto perspiciatis, tempora ipsa, culpa officia nobis vel quasi atimpedit odio? Minus quos dolor doloremque commodi sed.',
+            'author' : 'Brad Traversy',
+            'create_date' : '09-09-2023'
+        },
+        {
+            'id' : 2,
+            'title' : 'Article Two',
+            'body' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae provident vero magni architecto perspiciatis, tempora ipsa, culpa officia nobis vel quasi atimpedit odio? Minus quos dolor doloremque commodi sed.',
+            'author' : 'Brad Traversy',
+            'create_date' : '03-09-2023'
+        },
+        {
+            'id' : 3,
+            'title' : 'Article Three',
+            'body' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae provident vero magni architecto perspiciatis, tempora ipsa, culpa officia nobis vel quasi atimpedit odio? Minus quos dolor doloremque commodi sed.',
+            'author' : 'Brad Traversy',
+            'create_date' : '09-29-2023'
+        },
+    ]
+    return articles
